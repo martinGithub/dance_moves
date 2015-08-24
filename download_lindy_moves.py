249240eccbab1fef43ef48b05983bd60b5a17a1d
martinGithub <martindlg@yahoo.fr>
2015-08-24 16:03:53 +0200

    putting list of moves in alphetical order in the readme file

--- download_lindy_moves.py
+++ download_lindy_moves.py
@@ -7,8 +7,8 @@ import os.path
 download_folder = os.path.join('.','original_videos')
 target_folder   = os.path.join('.','edited_videos')
 rewriteAll=False # set this value to true if you want to recreate existing videos, or simply delet the existing videos in the file explorator
-f = open('listmoves.md', 'w')
-f.write('##Existing moves\n\n')
+
+movesUrlsDict=dict()
 
 def download_and_cut(videoid,start,end,subfolder,move_name):
     FMT = '%H:%M:%S'
@@ -34,8 +34,8 @@ def download_and_cut(videoid,start,end,subfolder,move_name):
     url='http://www.infinitelooper.com/?v=%s#/%d;%d'%(videoid ,s,e)
     url='https://www.yourepeat.com/watch/?v=%s&start_at=%d&end_at=%d'%(videoid ,s,e)
     #coul use http://loopthetube.com/#FB1cCoib7xQ&start=41.948&end=46.915
-   
-    f.write('* [%s](%s)\n'%(move_name,url))
+    movesUrlsDict[move_name]=url
+    
     
 
 download_and_cut('HYcuxW5_ilg','00:00:25','00:00:31','level 1/8 counts','arm catch 8 step')
@@ -68,19 +68,40 @@ download_and_cut('bv_Xsyy3uAY','00:00:58','00:01:04','level 1/6 counts','side pa
 download_and_cut('91FZSR9wQKk','00:00:55','00:01:01','level 1/6 counts','she goes he goes')
 download_and_cut('CF0KIsQR6A4','00:01:49','00:01:54','level 1/8 counts','promenade')
 download_and_cut('CF0KIsQR6A4','00:01:56','00:02:01','level 1/8 counts','reverse promenade')
-download_and_cut('USXmK5QXwCc','00:02:19','00:02:26','level 1/6 counts','cudle')
+download_and_cut('USXmK5QXwCc','00:02:19','00:02:26','level 1/6 counts','cudle a.k.a wrap-in wrap-out')
+
+
 download_and_cut('cjQIwvnfI3Q','00:02:21','00:02:29','level 1/6 counts','sweetheart')
-download_and_cut('USXmK5QXwCc','00:00:56','00:01:00','level 1/6 counts','belt slide')
+download_and_cut('USXmK5QXwCc','00:00:56','00:01:00','level 1/6 counts','belt slide a.k.a waist wrap')
 download_and_cut('yR-H2MKsOTM','00:02:28','00:02:36','level 1/8 counts','lindy hop basket')
 download_and_cut('hlkp-XMeQOM','00:01:05','00:01:22','level 1/8 counts','tandem charleston')
 download_and_cut('jU0NWSu2x0o','00:00:28','00:00:37','level 1/8 counts','tandem charleston2')
-download_and_cut('jU0NWSu2x0o','00:00:35','00:00:46','level 1/8 counts','tandem - Windshield wiper')
+download_and_cut('jU0NWSu2x0o','00:00:35','00:00:46','level 1/8 counts','tandem - windshield wiper')
 download_and_cut('jU0NWSu2x0o','00:00:49','00:00:57','level 1/8 counts','tandem - follower turn')
 download_and_cut('jU0NWSu2x0o','00:00:59','00:01:07','level 1/8 counts','tandem - push away exit')
 download_and_cut('-eQaOZXJdkA','00:00:19','00:00:30','level 1/8 counts','airplane charleston ')
 download_and_cut('5MEfGPNf3nE','00:00:33','00:00:43','level 1/8 counts','s-turn into tandem charleston')
 download_and_cut('0nbTtgY-aQ8','00:00:31','00:00:41','level 1/8 counts','scoots')
 download_and_cut('HYcuxW5_ilg','00:00:31','00:00:41','level 1/8 counts','scoots')
+download_and_cut('Dt4DYHQ-yYE','00:00:51','00:01:00','level 1/8 counts','twist around')
+download_and_cut('ZHdXSX0pbp4','00:00:21','00:00:29','level 1/8 counts','twist')
+download_and_cut('aoMux5a-32c','00:00:21','00:00:34','level 1/8 counts','traveling charleston')
+download_and_cut('mLkkUDXE65Y','00:02:20','00:02:26','level 2/8 counts','slide drag dig')
+download_and_cut('gnfTt8eqHgQ','00:00:34','00:00:47','level 2/8 counts','tandem - Side by Side Skates')
+
+download_and_cut('Hk3ICwPsgbU','00:00:48','00:00:54','level 2/8 counts','scissor kicks')
+
+download_and_cut('XsV7uIbozNA','00:01:14','00:01:19','level 2/8 counts','peel away')
+download_and_cut('YIiYOJWo87o','00:00:07','00:00:14','level 1/6 counts','pass by')
+download_and_cut('WWBWpZQ8ixs','00:02:32','00:02:39','level 1/8 counts','mini dip')
+download_and_cut('mLkkUDXE65Y','00:01:53','00:02:00','level 1/8 counts','jump out')
+download_and_cut('QRb9mO9DTY0','00:00:26','00:00:30','level 1/8 counts',"johnny's drop")
+
+f = open('listmoves.md', 'w')
+f.write('##Existing moves\n\n')
+for move_name in sorted(movesUrlsDict.keys()):
+    f.write('* [%s](%s)\n'%(move_name,movesUrlsDict[move_name]))
+f.close()
 
 
 
