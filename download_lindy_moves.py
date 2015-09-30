8f675516c27b364259125bde473dfb7723b72cb0
martinGithub <martindlg@yahoo.fr>
2015-09-30 09:51:45 +0200

    adding possibility to use local spreadsheet

--- download_lindy_moves.py
+++ download_lindy_moves.py
@@ -8,6 +8,7 @@ system=platform.system()
 import urllib2
 import subprocess
 import csv
+import sys
 download_folder = os.path.join('.','original_videos')
 target_folder   = os.path.join('.','edited_videos')
 rewriteAll=False # set this value to true if you want to recreate existing videos, or simply delet the existing videos in the file explorator
@@ -54,7 +55,7 @@ def download_and_cut(videoid,start,end,level,beatcount,move_name):
             subprocess.call(command,shell=True)
         
     else:
-        print 'video "%s.avi" already created'% move_name
+        print ' Video "%s.avi" already created'% move_name
     s=int((datetime.strptime(start, FMT)-datetime(1900,1,1)).total_seconds() ) 
     e=int((datetime.strptime(end, FMT)-datetime(1900,1,1)).total_seconds() ) 
     url='http://www.youtube.com/v/%s?start=%d&end=%d&autoplay=1&loop=1'%(videoid ,s,e)
@@ -62,24 +63,71 @@ def download_and_cut(videoid,start,end,level,beatcount,move_name):
     url='https://www.yourepeat.com/watch/?v=%s&start_at=%d&end_at=%d'%(videoid ,s,e)
     #coul use http://loopthetube.com/#FB1cCoib7xQ&start=41.948&end=46.915
     movesUrlsDict[move_name]={'url':url,'level':level,'beatcount':beatcount}
- 
-url = 'https://raw.githubusercontent.com/martinGithub/lindy_hop_moves/listmoves/lindy_moves.csv'
-csvfile = urllib2.urlopen(url)
-reader = csv.reader(csvfile, delimiter=str(','), quotechar=str('|'))
-for row in reader:
-    if not row==[]:
-        download_and_cut(*row)
-    else:
-        print 'empty row'       
 
-#download_and_cut('HYcuxW5_ilg','00:00:25','00:00:31','level 1/8 counts','arm catch 8 step')
-#with open('moves.csv', 'rb') as csvfile:
-    #reader = csv.reader(csvfile, delimiter=str(','), quotechar=str('|'))
-    #for row in reader:
-        #if not row==[]:
-            #download_and_cut(*row)
-        #else:
-            #print 'empty row'
+valid=False
+
+while not valid:
+    v=raw_input (' Do you want to download videos from:\n\n'+\
+           '  [1] the default list from '+\
+           'https://raw.githubusercontent.com/martinGithub/lindy_hop_moves/listmoves/lindy_moves.csv\n'+\
+           '  [2] another spreadsheet in the csv format available online\n'+\
+           '  [3] a list from a google doc spreadsheet \n'+\
+           '  [4] a spreasheet in the cvs format available locally on the harddisk \n\n'+\
+           ' Make your choice and press enter: ')
+    
+    valid=v in ['1','2','3','4']
+
+
+if v=='1' or v=='2':
+    if v=='1':
+        url = 'https://raw.githubusercontent.com/martinGithub/lindy_hop_moves/listmoves/lindy_moves.csv'
+    else:
+        url =raw_input('enter the url of the raw csv file')
+    csvfile = urllib2.urlopen(url)
+    reader = csv.reader(csvfile, delimiter=str(','), quotechar=str('|'))
+    first=True
+    for row in reader:
+        if first:#skip the first line that correspond to the columns names
+            first=False
+            continue
+        
+        if not row==[]:
+            download_and_cut(*row)
+        else:
+            print 'empty row'         
+elif v=='3':
+    spid =raw_input(' Enter the speadsheet id (part of the url following https://docs.google.com/spreadsheets/d/): ')
+    url = 'https://spreadsheets.google.com/feeds/list/'+spid+'/default/public/values?alt=json'
+    print ' Getting data from '  +url 
+        
+    
+    jsonfile = urllib2.urlopen(url)
+    import json       
+    jsondata=json.loads(jsonfile.read())
+    for move in jsondata['feed']['entry']:    
+        download_and_cut(videoid=move['gsx$youtubeid']['$t'],
+                         start=move['gsx$start']['$t'], 
+                         end=move['gsx$end']['$t'], 
+                         level=move['gsx$level']['$t'],
+                         beatcount=move['gsx$beatcount']['$t'],
+                         move_name=move['gsx$name']['$t'])
+   
+elif v=='4':    
+    filepath =raw_input(' Enter the path and name of of the csv file (for example: ./exampleMoves.csv )')
+    with open(filepath, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=str(','), quotechar=str('|'))
+        column_names=reader.next()
+        column_names[column_names.index('name')]='move_name'
+        column_names[column_names.index('youtubeid')]='videoid'
+        for row in reader:
+            if not row==[]:
+                d=dict(zip(column_names, row))
+                d2=dict()
+                for k in 'videoid','start','end','level','beatcount','move_name':
+                    d2[k]=d[k]                
+                download_and_cut(**d2)
+            else:
+                print 'empty row'
             
 #with  open('listmoves.md', 'r') as f:
 
