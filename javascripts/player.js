ac163aea9659edfa5760d1dbaf6c13bd5b4097d9
martinGithub <martindlg@yahoo.fr>
2015-10-31 17:22:47 +0100

    improving javascript quality by reducing duplicated code and allows the use of columns after a column with name x to be displayed below the video

--- javascripts/player.js
+++ javascripts/player.js
@@ -84,8 +84,122 @@
 	      }
 
 
+	function createMoveTable(columns,listmoves){
+			var text='<table style="width:100%" class="sortable">  '
+
+
+
+
+
+			var column_to_ignore={};
+			var column_show_below={};
+			var column_to_hide={};
+
+			hide=false
+			for	(i = 0; i < columns.length; i++) {
+			column_to_ignore[columns[i]]=0;
+			column_show_below[columns[i]]=0;
+			column_to_hide[columns[i]]=0;
+			
+				if (hide)
+				{
+				column_to_hide[columns[i]]=1;
+				column_to_ignore[columns[i]]=1;
+				column_show_below[columns[i]]=1;
+				}
+			if (columns[i]==='x') // every column after that columns will appear below the video
+			{	hide=true
+				column_to_hide[columns[i]]=1;
+				column_to_ignore[columns[i]]=1;
+				column_show_below[columns[i]]=0;
+			}
+
+			}
+			column_to_ignore['start']=1;
+			column_to_ignore['end']=1;
+			column_to_ignore['youtubeid']=1;
+			column_to_ignore['name']=1;
+			column_to_ignore['dance']=1;
+			column_to_ignore['comments']=1;
+			column_show_below['start']=0;
+			column_show_below['end']=0;
+			column_show_below['youtubeid']=0;
+			column_show_below['name']=0;
+			column_show_below['dance']=0;
+			text+= '<tr><th>name</th>'
+
+			sub_column_names=[]
+			sub_column_displaynames=[]
+			sub_column_names.push('name')
+			sub_column_displaynames.push('name')
+
+
+			for	(i = 0; i < columns.length; i++) {
+					
+
+					if (column_to_ignore[columns[i]]==0){
+					text+='<th >'+columns[i]+'</th>'
+					sub_column_names.push(columns[i])
+					sub_column_displaynames.push(columns[i])
+					}
+					if (column_to_hide[columns[i]]==1){
+					text+='<th hidden>'+columns[i]+'</th>'
+					sub_column_names.push(columns[i])
+					sub_column_displaynames.push(columns[i]);
+					}
+					}
+			text+='</tr>'
+	
+		
+			for	(index = 0; index < listmoves.length; index++) {
+				move=listmoves[index]
+				text+= '<tr>'
+			    	text +=   "<td><a class='croppedVideo' videoid='"+move['youtubeid']
+				text+="' start="+move['start']+" end="+move['end']+" href='#'>"+move['name']+"</a></td>"
+				for	(i = 0; i < columns.length; i++) {
+	
+						if (column_to_ignore[columns[i]]==0){
+						text+='<td>'+move[columns[i]]+'</td>'
+						}
+						if (column_to_hide[columns[i]])
+							{text+='<td hidden>'+move[columns[i]]+'</td>'}
+					
+
+					}
+
+			   	
+				text+='</tr>'
+			  }
+			text+=  "</table> "
+			$("#listmoves").html(text);
+		
+			$('a.croppedVideo').on("click", function (e) {
+				e.preventDefault();
+				player.loadVideoById($(this).attr('videoid'));
+				
+				maxtime=getSeconds($(this).attr('end'));
+				mintime=getSeconds($(this).attr('start'));
+				console.log($(this).attr('videoid')+ ' '+ mintime+ ' '+mintime)
+				$("#title").html($(this).text()); 
+				player.seekTo(mintime, 0);
+				idbeat=1
+				comments=''
+				$(this).parent().parent().children('td').each(function( index ) {
+  				console.log( index +":"+ sub_column_names[index]+": " + $( this ).text() );
+				if (column_show_below[sub_column_names[index]]){
+				comments+='<div>'+'<span class="sub_column_name">'+sub_column_displaynames[index]+': '+'</span>'+$( this ).text()+'</div>' }
+				});
+				$("#video_comments").html(comments)
+				
+			});
+
+			}
+
 
 	$(document).ready(function() {
+
+
+
 
 
 if ($.urlParam('source')=='google_spreadsheet')
@@ -96,59 +210,31 @@ $.ajax({
 	  dataType: "json",
 	  url: 'https://spreadsheets.google.com/feeds/list/'+$.urlParam('id')+'/default/public/values?alt=json',
 		success: function(json) {    
-			listmoves=json.feed.entry   
-			var text='<table style="width:100%" class="sortable">  '
+			listmovesJson=json.feed.entry   
+
+
+			
 			var columns=[]
 
-			for (k in listmoves[0]){
-if (k.substring(0, 4)==='gsx$'){
+			for (k in listmovesJson[0]){
+			if (k.substring(0, 4)==='gsx$'){
 					columns.push(k.substring(4))
-console.log(k.substring(4))}
+					console.log(k.substring(4))}
 				}
-			var column_to_ignore={};
-			for	(i = 0; i < columns.length; i++) {
-			column_to_ignore[columns[i]]=0;
-			}
-			column_to_ignore['start']=1;
-			column_to_ignore['end']=1;
-			column_to_ignore['youtubeid']=1;
-			column_to_ignore['name']=1;
-			column_to_ignore['dance']=1;
-			column_to_ignore['comments']=1;
-text+= '<tr><th>name</th>'
-
-	for	(i = 0; i < columns.length; i++) {
-					if (column_to_ignore[columns[i]]==0){
-					text+='<th>'+columns[i]+'</th>'
-					}
-					}
-text+='</tr>'
-			for	(index = 0; index < listmoves.length; index++) {
-				move=listmoves[index]
-				text+= '<tr>'
-			    	text +=   "<td><a class='croppedVideo' videoid='"+move.gsx$youtubeid.$t
-				text+="' start="+move.gsx$start.$t+" end="+move.gsx$end.$t+" href='#'>"+move.gsx$name.$t+"</a></td>"
+			listmoves=[]
+			for	(index = 0; index < listmovesJson.length; index++) {
+				moveJson=listmovesJson[index]
+				move={}
 				for	(i = 0; i < columns.length; i++) {
-					if (column_to_ignore[columns[i]]==0){
-					text+='<td>'+move['gsx$'+columns[i]].$t+'</td>'
+					move[columns[i]]=moveJson['gsx$'+columns[i]].$t
 					}
-					}
-			   	
-				text+='</tr>'
-			  }
-			text+=  "</table> "
-			$("#listmoves").html(text); 
-		
-			$('a.croppedVideo').on("click", function (e) {
-				e.preventDefault();
-				player.loadVideoById($(this).attr('videoid'));
-				maxtime=getSeconds($(this).attr('end'));
-				mintime=getSeconds($(this).attr('start'));
-				console.log($(this).attr('videoid')+ ' '+ mintime+ ' '+mintime)
-				$("#title").html($(this).text()); 
-				player.seekTo(mintime, 0);
-				idbeat=1
-			});},
+				
+				listmoves.push(move);
+			}
+
+			createMoveTable(columns,listmoves);
+
+			},
     		error: function(e) {
        			console.log(e.message);}
 	});
@@ -163,62 +249,22 @@ spreadsheet_link=$('#playerwithplaylist').attr('spreadsheet');
 	  url: $('#playerwithplaylist').attr('rawspreadsheet')+'?ref=listmoves',
 		success: function(json) {       
 			//$("#listmoves").text('worked?'); 
-			var listmoves=$.csv.toArrays(atob(json.content));
+			var listmovesCSV=$.csv.toArrays(atob(json.content));
 			var text='<table style="width:100%" class="sortable">  <tr>'
-			columns=listmoves[0]
-	var associativeArray = {};
-for	(i = 0; i < columns.length; i++) {
-associativeArray[columns[i]]=i;
-}
+			columns=listmovesCSV[0]
+			listmoves=[]
 
-var column_to_ignore={};
-for	(i = 0; i < columns.length; i++) {
-column_to_ignore[columns[i]]=0;
-}
-column_to_ignore['start']=1;
-column_to_ignore['end']=1;
-column_to_ignore['youtubeid']=1;
-column_to_ignore['name']=1;
-column_to_ignore['dance']=1;
-column_to_ignore['comments']=1;
+			for	(index = 1; index < listmovesCSV.length; index++) {
+				
+				movecsv=listmovesCSV[index]
+				move={}
+				for	(i = 0; i < columns.length; i++) {
+				move[columns[i]]=movecsv[i]}
+				listmoves.push(move)
+			}
+			createMoveTable(columns,listmoves);
 
-text+= '<tr><th>name</th>'
-
-	for	(i = 0; i < columns.length; i++) {
-					if (column_to_ignore[columns[i]]==0){
-					text+='<th>'+columns[i]+'</th>'
-					}
-					}
-text+='</tr>'
-
-			for	(index = 1; index < listmoves.length; index++) {
-				move=listmoves[index]
-				text+= '<tr>'
-			    	text +=   "<td><a class='croppedVideo' videoid='"+move[associativeArray['youtubeid']]
-				text+="' start="+move[associativeArray['start']]+" end="+move[associativeArray['end']]+" href='#'>"+move[associativeArray['name']]+
-
-"</a></td>";
-for	(i = 0; i < columns.length; i++) {
-if (column_to_ignore[columns[i]]==0){
-text+='<td>'+move[i]+'</td>'
-}
-}
-
-				text+='</tr>'
-			  }
-			text+=  "</tr></table> "
-			$("#listmoves").html(text); 
-		
-			$('a.croppedVideo').on("click", function (e) {
-				e.preventDefault();
-				player.loadVideoById($(this).attr('videoid'));
-				maxtime=getSeconds($(this).attr('end'));
-				mintime=getSeconds($(this).attr('start'));
-				console.log($(this).attr('videoid')+ ' '+ mintime+ ' '+mintime)
-				$("#title").html($(this).text()); 
-				player.seekTo(mintime, 0);
-				idbeat=1
-			});},
+			},
     		error: function(e) {
        			console.log(e.message);}
 	});
@@ -228,7 +274,7 @@ text+='<td>'+move[i]+'</td>'
 
 if (typeof spreadsheet_link !== typeof undefined && spreadsheet_link !== false) {
 
-$("#playerwithplaylist").html("<div id='left'> <h3 id='title'>click on a video on the right<h3>  <div id='player'></div>  <button type='button'  class='speedButton' speed=1>normal speed</button>  <button type='button' class='speedButton' speed=0.5 >slow x2</button>  <button type='button' class='speedButton' speed=0.25 >slow x4</button> <!--<button type='button' class='BPM'  >click on 1</button>--> <p class='video_comments'></p></div><div id='right'> You can create your own playlist by following the instructions  <a href='../newplaylist.html'>here</a> You can go <a href='https://github.com/martinGithub/dance_moves'>here</a> to get a python script that download the edited videos.You can contribute to this list by editing the file <a class='spreadsheet_link'>here</a>  once you got the permissions from the owner of the file.<br> <br><div id='listmoves'></div></div>'");
+$("#playerwithplaylist").html("<div id='left'> <h3 id='title'>click on a video on the right</h3>  <div id='player'></div>  <button type='button'  class='speedButton' speed=1>normal speed</button>  <button type='button' class='speedButton' speed=0.5 >slow x2</button>  <button type='button' class='speedButton' speed=0.25 >slow x4</button>  <p id='video_comments'></p></div><div id='right'> You can create your own playlist by following the instructions  <a href='../newplaylist.html'>here</a> You can go <a href='https://github.com/martinGithub/dance_moves'>here</a> to get a python script that download the edited videos.You can contribute to this list by editing the file <a class='spreadsheet_link'>here</a>  once you got the permissions from the owner of the file.<br> <br><div id='listmoves'></div></div>'");
 $('a.spreadsheet_link').attr('href',spreadsheet_link)
 }
 
